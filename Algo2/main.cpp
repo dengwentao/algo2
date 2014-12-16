77795125a2cd6c7ccb6984cbed6cfe7a2cee7652
Wentao Deng <wdeng@groupon.com>
2014-12-15 19:41:15 -0800

    Initial Commit

---
+++ Algo2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algo2
+//
+//  Created by Wentao Deng on 12/15/14.
+//  Copyright (c) 2014 WENTAO INC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
